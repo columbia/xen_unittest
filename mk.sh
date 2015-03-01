364ed75d5d69953234286d9bb0c1bf8b9ffcd43c
Jintack <jintack@cs.columbia.edu>
2015-02-28 22:38:55 -0500

    Add make script for Seattle

--- mk.sh
+++ mk.sh
@@ -1,7 +1,35 @@
+#!/bin/bash
 #make dist-xen XEN_TARGET_ARCH=arm32 CROSS_COMPILE=arm-linux-gnueabihf-
-make dist-xen
-sudo cp xen/xen /boot/efi/xen/xen.gz
-sync
-sudo md5sum xen/xen /boot/efi/xen/xen.gz
 
+if [[ -f .localconf ]]; then
+        source .localconf
+else
+	ARCH="arm64"
 
+        echo -n "What's the architecture? [$ARCH]:"
+        read _ARCH
+        if [[ -n "$_ARCH" ]]; then
+               TARCH="$_ARCH"
+        fi
+	echo "ARCH=\"$ARCH\"" > .localconf
+fi
+
+if [[ "$ARCH" == "arm64" ]]; then
+	CROSS_COMPILER=aarch64-linux-gnu-
+	'''
+else if [[ "$ARCH" == "arm32" ]]; then
+	CROSS_COMPILER=arm-linux-gnueabihf-
+	'''
+fi
+
+make dist-xen XEN_TARGET_ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILER
+
+echo "copy to?"
+read _COPYTO
+
+if [[ "$_COPYTO" == "y" ]]; then
+	md5sum xen/xen
+	scp xen/xen s2r:/boot/efi/xen/xen.gz
+	ssh s2r sync
+	ssh s2r md5sum /boot/efi/xen/xen.gz
+fi
